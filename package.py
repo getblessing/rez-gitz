71761d72b4626277936e35885c229e71ee93282c
David Lai <davidlatwe@gmail.com>
2020-08-09 20:10:19 +0800

    Add python as required, fix linter err

--- package.py
+++ package.py
@@ -9,6 +9,7 @@ version = "0.1.1"
 
 requires = [
     "rez-2.29+",
+    "python",
 ]
 
 tools = [
@@ -19,5 +20,6 @@ build_command = "python {root}/rezbuild.py {install}"
 
 
 def commands():
+    env = globals()["env"]
     env.PATH.prepend("{root}/payload/bin")
     env.PYTHONPATH.prepend("{root}/payload")
